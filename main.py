c69436a623c8409779945c2b3991d0aa6822ed14
Oscar-RES <oscar.res2002@gmail.com>
2022-11-06 03:41:04 +0100

    Added a Splash Screen yay

--- main.py
+++ main.py
@@ -4,14 +4,29 @@ from kivy.properties import StringProperty
 from kivymd.uix.card import MDCard
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.uix.scrollview import ScrollView
+from kivy.clock import Clock
+#Window.size(310,580)
 
 class MainApp(MDApp):
     def build(self):
+        global sm
         self.theme_cls.material_style = "M3"
         sm = ScreenManager()
+        sm.add_widget(SplashScreen(name='splash'))
         sm.add_widget(HomeScreen(name='home'))
         sm.add_widget(ChallengesScreen(name='challenges'))
+        #Clock.schedule_once(HomeScreen)
         return sm
+    
+        
+      
+
+class SplashScreen(Screen):
+    def switch(self, *args):
+        self.parent.current = 'home'
+    
+    def on_enter(self, *args):
+        Clock.schedule_once(self.switch,1)
    
 class HomeScreen(Screen):
     pass
@@ -27,5 +42,6 @@ class ElementCard(MDCard):
     image = StringProperty()
     items_count = StringProperty()
     subtext = StringProperty()
+
     
 MainApp().run()
