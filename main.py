c66888dfaff4260c176d413f38e5065c8a9bbb9d
Janst1000 <janst1000@gmail.com>
2022-11-06 01:13:48 +0100

    added Screen manager

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.properties import StringProperty
 from kivymd.uix.card import MDCard
+from kivy.uix.screenmanager import ScreenManager, Screen
+
 
 class MainApp(MDApp):
     def build(self):
         self.title='KivyMD Dashboard'
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "DeepPurple"
+
+        sm = ScreenManager()
+        sm.add_widget(HomeScreen(name='home'))
+        sm.add_widget(ChallengesScreen(name='challenges'))
+        return sm
+
+class HomeScreen(Screen):
+    pass
+
+class ChallengesScreen(Screen):
+    pass
    
 class ElementCard(MDCard):
     text = StringProperty()
